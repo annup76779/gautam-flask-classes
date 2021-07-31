6f9e1111ef8eb8dd62c2e5118b28077e35e125a9
CompBook <annup76779@gmail.com>
2021-07-31 19:21:33 +0530

    partially added the register and login to application

--- model/model.py
+++ model/model.py
@@ -1,5 +1,7 @@
+from enum import unique
 from wsgi import db
 from datetime import datetime
+from passlib.hash import sha256_crypt
 
 
 class User(db.Model):
@@ -11,6 +13,12 @@ class User(db.Model):
     """
 
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    user_email = db.Column(db.String(200), nullable = False)
-    password = db.Column(db.String(38), nullable = False)
+    user_email = db.Column(db.String(200), nullable = False, unique = True)
+    password = db.Column(db.String(80), nullable = False)
     data_created = db.Column(db.DateTime, nullable=False, default = datetime.now())
+
+    def hash_password(self, password):
+        self.password = sha256_crypt.hash(password)
+
+    def verify_password(self, password):
+        return sha256_crypt.verify(password, self.password)
--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, sqlalchemy # object relational mapper ORM
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///main.db"
 
-from model.model import *
+from model.model import User
 
 """
     there are few request methods when you work http protocal
@@ -20,9 +20,62 @@ from model.model import *
     DELETE - it is used to delete any data in database.
 
 """
-
+# { REST-API }
 
 @app.route("/register", methods=["POST"])
 def register():
-    if request.method == "POST":
-        pass
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        if email and password:
+            # we will save data in database
+            user = User(user_email=email)
+            user.hash_password(password)
+
+            db.session.add(user)
+            db.session.commit()
+
+            return jsonify(status = True, msg="User registered.")
+        
+        return jsonify(status = False, 
+                msg = "Email is missing!" if email is None else "Password is missing!"
+            )
+
+
+    except sqlalchemy.exc.IntegrityError as e:
+        print(e)
+        return jsonify(status = False, msg="email is already registered.")
+    except:
+        db.session.rollback()
+        return jsonify(
+            status = False, 
+            msg = "Unknown Error Occured!"
+        )
+
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    try:
+        email, password = request.form.get("email"), request.form.get("password")
+        if email and password:
+            #verifying the user
+            "SELECT password FROM myappusers WHERE user_email=?"
+            "SELECT * FROM myappusers WHERE user_email=? LIMIT=1"
+            "SELECT * FROM myappusers WHERE id=?"
+            # user = User.query.get(2)
+            user = User.query.filter_by(user_email = email).first()
+            if user.verify_password(password):
+                return jsonify(status = True, msg = "Logged in!")
+            else:
+                return jsonify(status =False, msg = "password didn't matched!")
+            # user = User.query.with_entities(User.password, User.user_email).filter_by(user_email = email)
+
+        else:
+            return jsonify(status = False, msg = "Email is missing!" if email is None else "Password is missing!")
+
+    except Exception as e:
+        print(e,"\n")
+        return jsonify(status = False, msg="Unknown error occured!")
+
